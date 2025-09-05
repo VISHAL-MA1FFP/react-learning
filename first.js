8c7ea4841ee175a073ea1fa3661e5960f73dc418
vishal sharma <vishalsharma9283@gmail.com>
2025-09-05 13:37:23 -0400

    file2

--- first.js
+++ first.js
@@ -1,9 +1,13 @@
 // creating the element by react
 
+
+
 const element=React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"orange",fontSize:"30px"}},"hello tess")
 const element1=React.createElement("h2",{id:"second",className:"one",style:{backgroundColor:"orange",fontSize:"30px"}},"let's do it")
 
 // now we will put these two in a div and then push it
+
+
 const div=React.createElement('div',{},[element,element1])
 // now add in the dom
 
