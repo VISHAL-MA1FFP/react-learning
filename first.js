7091702fc92c99723ff983abbf01234470e52938
vishal sharma <vishalsharma9283@gmail.com>
2025-09-06 13:04:13 -0400

    three

--- first.js
+++ first.js
@@ -1,6 +1,7 @@
 const element1=React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello world")
 const element= React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello tess")
-const div=React.createElement('div',{},[element1,element])
+const element2= React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello tess")
+const div=React.createElement('div',{},[element1,element,element2])
 
 const root=document.getElementById("root")
 const r=ReactDOM.createRoot(root)
