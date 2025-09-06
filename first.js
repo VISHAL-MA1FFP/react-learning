ea4a87c11176453d8771e66882315f356346e8dd
vishal sharma <vishalsharma9283@gmail.com>
2025-09-06 12:51:31 -0400

    one1

--- first.js
+++ first.js
@@ -1,5 +1,7 @@
 const element1=React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello world")
+const element= React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello tess")
+const div=React.createElement('div',{},[element1,element])
 
 const root=document.getElementById("root")
 const r=ReactDOM.createRoot(root)
-r.render(element1)
+r.render(div)
