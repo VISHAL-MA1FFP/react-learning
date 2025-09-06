224fced4a76037c8ea76dcf5d6b5fb8d2eed881c
vishal sharma <vishalsharma9283@gmail.com>
2025-09-06 12:43:40 -0400

    new1

--- first.js
+++ first.js
@@ -1,20 +1,5 @@
-// creating the element by react
+const element1=React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"black",color:"white"}},"hello world")
 
-
-
-const element=React.createElement("h1",{id:"first",className:"one",style:{backgroundColor:"orange",fontSize:"30px"}},"hello tess")
-const element1=React.createElement("h2",{id:"second",className:"one",style:{backgroundColor:"orange",fontSize:"30px"}},"let's do it")
-
-// now we will put these two in a div and then push it
-
-
-const div=React.createElement('div',{},[element,element1])
-// now add in the dom
-
-const root=document.getElementById('root')
-
+const root=document.getElementById("root")
 const r=ReactDOM.createRoot(root)
-//r.render(element)
-// r.render(element1)
-
-r.render(div)
+r.render(element1)
